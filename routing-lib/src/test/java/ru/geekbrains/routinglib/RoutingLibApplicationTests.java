14da599621dffcc1f1e67fa9805d48892056a679
Evengarr <Ravasect@gmail.ru>
2021-08-22 15:31:19 +0300

    Initial commit

---
+++ routing-lib/src/test/java/ru/geekbrains/routinglib/RoutingLibApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.routinglib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutingLibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
